9a1ba712b46daa96e446f40bbf68e87c4fe8e923
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-20 17:06:21 +0530

    created no args and parametrized constructor in album

--- src/main/java/com/niit/jdp/model/Album.java
+++ src/main/java/com/niit/jdp/model/Album.java
@@ -13,4 +13,13 @@ public class Album {
     private String nameOfAlbum;
     private String genreOfAlbum;
     private List<Song> songs;
+
+    public Album() {
+    }
+
+    public Album(String nameOfAlbum, String genreOfAlbum, List<Song> songs) {
+        this.nameOfAlbum = nameOfAlbum;
+        this.genreOfAlbum = genreOfAlbum;
+        this.songs = songs;
+    }
 }
