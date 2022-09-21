484a101ab0ee802f5ead6c0c115508702422037c
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-21 18:04:22 +0530

    interface Repository is completed with common methods to override

--- src/main/java/com/niit/jdp/repository/Repository.java
+++ src/main/java/com/niit/jdp/repository/Repository.java
@@ -1,4 +1,15 @@
 package com.niit.jdp.repository;
 
-public interface Repository {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Repository<T> {
+    boolean add(Connection connection, T object) throws SQLException;
+
+    List<T> getAll(Connection connection) throws SQLException;
+
+    T getById(Connection connection, int id) throws SQLException;
+
+    boolean deleteById(Connection connection, int id) throws SQLException;
 }
