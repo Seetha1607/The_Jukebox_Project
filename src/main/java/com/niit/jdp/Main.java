f0373686ea0af56a49130016c261e9e8fa628f30
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-23 09:39:33 +0530

    created jukebox catalogue in main class

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -4,10 +4,10 @@ import com.niit.jdp.model.Playlist;
 import com.niit.jdp.repository.PlaylistRepository;
 import com.niit.jdp.repository.SongRepository;
 import com.niit.jdp.service.database.DatabaseService;
+import com.niit.jdp.service.database.MusicPlayerService;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -18,8 +18,8 @@ public class Main {
             System.out.println("Welcome to the JukeBox Catalogue");
             System.out.println("============================================");
             System.out.println("1. View all playlist");
-            System.out.println("2. Create playlist");
-            System.out.println("3: View all Songs from playlist");
+            System.out.println("2. Search song by playlist ID");
+            System.out.println("3: Create playlist using existing songs");
             System.out.println("4. Play song from playlist");
             System.out.println("5. Exit");
             System.out.println("============================================");
@@ -31,7 +31,6 @@ public class Main {
             DatabaseService databaseService = new DatabaseService();
             try {
                 databaseService.connect();
-                databaseService.printConnectionStatus();
                 SongRepository songRepository = new SongRepository();
                 PlaylistRepository playlistRepository = new PlaylistRepository();
                 Connection connection = databaseService.getConnection();
@@ -40,8 +39,7 @@ public class Main {
                     case 1:
 
                         System.out.println("View all Playlist");
-                        List<Playlist> all = playlistRepository.getAll(connection);
-                        System.out.println("all = " + all);
+                        playlistRepository.getAll(connection).forEach(System.out::println);
 
                         System.out.println("Enter id:");
                         int id = scanner.nextInt();
@@ -55,23 +53,19 @@ public class Main {
 
                     case 2:
 
-                        //System.out.println("Enter playlist id to create you're own playlist : ");
-                        //int ownPlaylist = scanner.nextInt();
-                        //Song song = new Song(ownPlaylist, "Naan Pizhai", "Vignesh Shivan", "Melody", "00.04.45","src/main/resources/songs/naan-pizhai-kaathuvaakula-rendu-kaadhal-128-kbps-sound.wav");
-                        //songRepository.add(connection,song);
-                        //System.out.println("song = " + song);
-                        boolean b = songRepository.deleteById(connection, 16);
-                        System.out.println("b = " + b);
-
-
                         break;
 
                     case 3:
-
                         System.out.println("View all Songs from Playlist");
                         songRepository.getAll(connection).forEach(System.out::println);
                         break;
 
+                    case 4:
+                        SongRepository songRepository1 = new SongRepository();
+                        songRepository1.getAll(connection).forEach(System.out::println);
+                        MusicPlayerService musicPlayerService = new MusicPlayerService();
+                        String songPath = scanner.next();
+                        musicPlayerService.play(connection, songPath);
                 }
             } catch (ClassNotFoundException | SQLException exception) {
                 System.err.println("Could not connect to the database!");
